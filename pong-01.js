6ac0fc9ce2f9d3a930531ea6a08feee9d392fa2e
martaquintana <43533605+martaquintana@users.noreply.github.com>
2019-03-07 10:52:21 +0100

    Bola movimiento

--- pong-01.js
+++ pong-01.js
@@ -6,6 +6,13 @@ function main()
   canvas.width = 600;
   canvas.height = 400;
   var ctx = canvas.getContext("2d");
+  window.onkeydown = (e) => {
+    e.preventDefault();
+    console.log(e.key)
+    if(e.key =='a'){
+      console.log("Tecla a apretada");
+    }
+  }
 
   //-- Raquetas
   ctx.fillStyle = 'white';
@@ -13,29 +20,86 @@ function main()
   ctx.fillStyle = 'white';
   ctx.fillRect(500,canvas.height/2-20, 10, 40)
   //--Linea
-  var linea = canvas.getContext("2d");
-  linea.setLineDash([4, 14, 18]);
-  linea.moveTo(canvas.width/2, 0);
-  linea.lineTo(canvas.width/2,canvas.height);
-  linea.strokeStyle = 'white';
-  linea.stroke();
+  ctx = canvas.getContext("2d");
+  ctx.setLineDash([4, 14, 18]);
+  ctx.moveTo(canvas.width/2, 0);
+  ctx.lineTo(canvas.width/2,canvas.height);
+  ctx.strokeStyle = 'white';
+  ctx.stroke();
   //--Pelota
-  var pelota = canvas.getContext("2d");
-  pelota.beginPath();
-  pelota.arc(100, 50, 5, 0, 2 * Math.PI);
-  pelota.stroke()
-  pelota.fillStyle = 'white';
-  pelota.fill()
+  ctx.beginPath();
+  ctx.arc(100, 50, 5, 0, 2 * Math.PI);
+  ctx.stroke()
+  ctx.fillStyle = 'white';
+  ctx.fill()
   //--Marcador Jugador1
-  var marcador1 = canvas.getContext("2d");
-  marcador1.font="60px Comic Sans MS";
-  marcador1.fillStyle = "white";
-  marcador1.textAlign = "center";
-  marcador1.fillText("2", canvas.width/2-50, 50);
+  ctx.font="60px Comic Sans MS";
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.fillText("2", canvas.width/2-50, 50);
   //--Marcador Jugador2
-  var marcador2 = canvas.getContext("2d");
-  marcador2.font="60px Comic Sans MS";
-  marcador2.fillStyle = "white";
-  marcador2.textAlign = "center";
-  marcador2.fillText("0", canvas.width/2+50, 50);
+  ctx.font="60px Comic Sans MS";
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.fillText("0", canvas.width/2+50, 50);
+
+  var bola = {
+    x_init:50,
+    y_init:50,
+    x:0,
+    y:0,
+    vx:4,
+    vy:1,
+    ctx:null,
+    ratio:5,
+
+    reset: function(){
+      this.x= this.x_init;
+      this.y= this.y_init;
+    },
+
+    init : function(ctx){
+      this.reset()
+      this.ctx= ctx;
+    },
+
+    draw : function(){
+      ctx.beginPath();
+      ctx.arc(this.x,this.y, this.ratio, 0, 2 * Math.PI);
+      ctx.stroke()
+      ctx.fillStyle = 'white';
+      ctx.fill()
+    },
+    update: function(){
+      this.x=this.x+this.vx;
+      this.y=this.y+this.vy;
+    },
+
+  }
+  bola.init(ctx);
+  bola.draw();
+  var timer = null;
+  var sacar = document.getElementById('sacar');
+  sacar.onclick = () => {
+    console.log("Sacar Click")
+    //Lanzar el timer si no estaba lanzado antes.
+    if(!timer){
+      timer = setInterval(()=>{
+        //--Actualizar la bola
+        bola.update()
+        //--Borrar canvas
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        //--Dibujar la bola
+        bola.draw()
+        //--Condiciones de terminacion
+        if(bola.x>canvas.width){
+          clearInterval(timer)
+          bola.reset();
+          bola.update();
+          ctx.clearRect(0,0,canvas.width,canvas.height);
+          bola.draw();
+        }
+      },20);
+    }
+  }
 }
